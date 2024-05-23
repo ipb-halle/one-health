cdc325b02a2f5e5e8d9df758773c0a6b6bf353c6
cav-ipb <vcardent@ipb-halle.de>
2024-05-23 16:07:10 +0200

    Change order of deployment to pass spring tests

--- run-docker.sh
+++ run-docker.sh
@@ -14,6 +14,9 @@ docker rm one-health-server
 docker rm one-health-client
 docker rm one-health-graphdb
 
+docker run --network one-health-network --name one-health-graphdb -p 7474:7474 -p 7687:7687 -e NEO4J_AUTH=none -v /data/onehealth/data:/data -d neo4j
+sleep(10)
+
 # Build the server and client images
 echo "run-docker.sh: Building images"
 docker build -t one-health-server:latest -f server.dockerfile --no-cache .
@@ -21,7 +24,6 @@ docker build -t one-health-client:latest -f client.dockerfile --no-cache .
 
 # Run the containers
 echo "run-docker.sh: Starting containers"
-docker run --network one-health-network --name one-health-graphdb -p 7474:7474 -p 7687:7687 -e NEO4J_AUTH=none -v /data/onehealth/data:/data -d neo4j
 docker run --network one-health-network --name one-health-server -p 8080:8080 -d one-health-server
 docker run --name one-health-client -p 3000:80 -d one-health-client
 
