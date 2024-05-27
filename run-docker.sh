b72a355a3296edcbb7c3c08a1d1b4de0dad44246
cav-ipb <vcardent@ipb-halle.de>
2024-05-27 10:23:23 +0200

    Add postgres to deployment pipeline

--- run-docker.sh
+++ run-docker.sh
@@ -14,15 +14,16 @@ docker rm one-health-server
 docker rm one-health-client
 docker rm one-health-graphdb
 
-docker run --network one-health-network --name one-health-graphdb -p 7474:7474 -p 7687:7687 -e NEO4J_AUTH=none -v /data/onehealth/data:/data -d neo4j
-
 # Build the server and client images
 echo "run-docker.sh: Building images"
+docker build -t rdkit-postgre:latest -f rdkit.dockerfile
 docker build -t one-health-server:latest -f server.dockerfile --no-cache .
 docker build -t one-health-client:latest -f client.dockerfile --no-cache .
 
 # Run the containers
 echo "run-docker.sh: Starting containers"
+docker run --network one-health-network --name one-health-graphdb -e NEO4J_AUTH=none -v /data/onehealth/neo4j_data:/data -d neo4j
+docker run --newtork one-health-network --name one-health-postgredb -e  POSTGRES_USER=one_health_user -e  POSTGRES_PASSWORD=one_health_password -e  POSTGRES_DB: one_health -v /data/onehealth/postgre_data:/var/lib/postgresql/data -d rdkit-postgre
 docker run --network one-health-network --name one-health-server -p 8080:8080 -d one-health-server
 docker run --name one-health-client -p 3000:80 -d one-health-client
 
