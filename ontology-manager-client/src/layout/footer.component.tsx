9de063565f37001922fcd1da2d5269f8a40dc3ca
cav-ipb <vcardent@ipb-halle.de>
2024-06-01 17:36:41 +0200

    Fix footer styles

--- ontology-manager-client/src/layout/footer.component.tsx
+++ ontology-manager-client/src/layout/footer.component.tsx
@@ -9,13 +9,13 @@ import './footer.component.scss';
 
 const Footer: React.FC = () => {
     return (
-        <div className='row app-footer col-12' style={{paddingTop: '10px', paddingBottom: '10px', paddingLeft: '10px', paddingRight: '10px'}}>
-            <div className='col-8'>
+        <div className='row app-footer'>
+            <div className='col-7'>
                 <div className='row'>
                     <div className='col-2'>
-                        <img alt="logo" src={logoIpb} height="60" style={{marginRight: '3px', marginLeft: '50px'}}></img>
+                        <img alt="logo" src={logoIpb} height="60" style={{marginRight: '3px'}}></img>
                     </div>
-                    <div className='col-8'>
+                    <div className='col-10' style={{display: "flex", alignItems: 'center'}}>
                         <p style={{fontSize: '8px', paddingTop: '20px'}}>
                             The Natural-One-Health service (n1h.org) is developed and operated by the Leibniz Institute of Plant Biochemistry. See
                             the <a href="https://www.ipb-halle.de/kontakt/impressum">imprint</a> for legal details.
@@ -23,8 +23,8 @@ const Footer: React.FC = () => {
                     </div>
                 </div>
             </div>
-            <div className='col-4'>
-                <img alt="logo" src={logoGlacier} height="60" style={{marginRight: '3px', marginLeft: '50px'}}></img>
+            <div className='col-5' style={{display: 'flex', alignItems: 'center'}}>
+                <img alt="logo" src={logoGlacier} height="60" style={{marginRight: '3px', marginLeft: 'auto'}}></img>
                 <img alt="logo" src={logoDaad} height="60" style={{marginRight: '3px'}}></img>
                 <img alt="logo" src={logoDip} height="40" style={{marginRight: '3px'}}></img>
                 <img alt="logo" src={logoLeibniz} height="60" style={{marginRight: '3px'}}></img>
