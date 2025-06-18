8aa54a9136eb829ed176edc98d7c161f20be1410
Frank Broda <fbroda@ipb-halle.de>
2025-06-18 13:58:56 +0200

    some fixes

--- run-build-service.sh
+++ run-build-service.sh
@@ -1,2 +1,9 @@
 docker build -t one-health-server:latest -f server.dockerfile --no-cache .
 docker build -t one-health-client:latest -f client.dockerfile --no-cache .
+
+#
+# Note to production environment:
+# the files generated during client build need to be copied manually to the 
+# proxy directory!
+#
+
