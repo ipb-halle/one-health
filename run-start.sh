8693bb6e8e6343bf9ba66dc50bc6b71368b7ee9f
Frank Broda <fbroda@ipb-halle.de>
2025-06-18 12:41:12 +0200

    Update run-start.sh
    Add network

--- run-start.sh
+++ run-start.sh
@@ -2,4 +2,4 @@ echo "run-docker.sh: Starting containers"
 docker run --network one-health-network --name one-health-graphdb -e NEO4J_AUTH=none -v $(pwd)/data/neo4j_data:/data -d neo4j
 docker run --network one-health-network --name one-health-postgredb -e  POSTGRES_USER=one_healt_huser -e  POSTGRES_PASSWORD=one_health_password -e  POSTGRES_DB=one_health -v $(pwd)/data/postgres_data:/var/lib/postgresql/data -d rdkit-postgre
 docker run --network one-health-network --name one-health-server -p 8080:8080 -d one-health-server
-docker run --name one-health-client -p 3000:80 -d one-health-client
+docker run --network one-health-network --name one-health-client -p 3000:80 -d one-health-client
