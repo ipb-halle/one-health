eb9fefa512fdedd42a62b5ad4515ef3a68bdc506
Hoda Anvari Kazemabad Local <halocal@compchem17.ipb-halle.de>
2026-05-27 10:34:04 +0200

    style(server): apply formatting fixes to controllers and DTOs

--- server/src/main/java/de/ipb_halle/server/api/interfaces/DataController.java
+++ server/src/main/java/de/ipb_halle/server/api/interfaces/DataController.java
@@ -20,20 +20,22 @@ public abstract class DataController<TDTO extends DTO<TKey>, TKey extends Compar
     }
 
     @PostMapping()
-    public ResponseEntity<TDTO> Create(@RequestBody TDTO dto){
+    public ResponseEntity<TDTO> Create(@RequestBody TDTO dto) {
         TDTO result = crudHandler.Create(dto);
-        if (result != null)
+        if (result != null) {
             return new ResponseEntity<>(result, HttpStatus.CREATED);
+        }
 
         return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
     }
 
     @PutMapping()
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<TDTO> Update(@RequestBody TDTO dto){
+    public ResponseEntity<TDTO> Update(@RequestBody TDTO dto) {
         TDTO result = crudHandler.Update(dto);
-        if (result != null)
+        if (result != null) {
             return new ResponseEntity<>(result, HttpStatus.CREATED);
+        }
 
         return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
     }
@@ -41,26 +43,26 @@ public abstract class DataController<TDTO extends DTO<TKey>, TKey extends Compar
     @GetMapping("{id}")
     public ResponseEntity<TDTO> Get(@PathVariable TKey id) {
         TDTO result = crudHandler.Get(id);
-        if (result != null)
+        if (result != null) {
             return new ResponseEntity<>(result, HttpStatus.OK);
+        }
 
         return new ResponseEntity<>(result, HttpStatus.NOT_FOUND);
     }
 
     @DeleteMapping("{id}")
-    public void Delete(@PathVariable TKey id){
+    public void Delete(@PathVariable TKey id) {
         crudHandler.Delete(id);
     }
 
-
     @GetMapping("all")
-    public ResponseEntity<List<TDTO>> Get(){
+    public ResponseEntity<List<TDTO>> Get() {
         List<TDTO> result = crudHandler.GetAll();
-        if (result != null)
+        if (result != null) {
             return new ResponseEntity<>(result, HttpStatus.OK);
+        }
 
         return new ResponseEntity<>(result, HttpStatus.NOT_FOUND);
     }
-
 
 }
