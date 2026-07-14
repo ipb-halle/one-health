a346d8f2d37afbf93c205b9f895f8d0236270030
Frank Broda <fbroda@ipb-halle.de>
2026-07-14 14:58:12 +0200

    extract footer icons into separate component

--- client/src/layout/footer.component.tsx
+++ client/src/layout/footer.component.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
-import logoGlacier from '../assets/logo-glacier.png';
-import logoDaad from '../assets/logo-daad.png';
-import logoDip from '../assets/logo-dip.png';
-import logoLeibniz from '../assets/logo-leibniz.png';
-import logoIpb from '../assets/logo-ipb.png';
-import logoForeign from '../assets/logo-foreign-office.png';
 import './footer.component.scss';
+import FooterIcons from './footerIcons';
 
 const Footer: React.FC = () => {
     return (
@@ -27,52 +22,8 @@ const Footer: React.FC = () => {
                 </ul>
             </div>
 
-            <div
-                className="app-footer-coloured-dark row fixed-bottom"
-                style={{ padding: '0px' }}>
-                <div className="col-7">
-                    <div className="col-2">
-                        <img
-                            alt="logo"
-                            src={logoIpb}
-                            height="60"
-                            style={{ marginRight: '3px' }}></img>
-                    </div>
-                </div>
-                <div
-                    className="col-5"
-                    style={{
-                        display: 'flex',
-                        justifyContent: 'end',
-                        alignItems: 'center',
-                    }}>
-                    <img
-                        alt="logo"
-                        src={logoGlacier}
-                        height="60"
-                        style={{ marginRight: '3px' }}></img>
-                    <img
-                        alt="logo"
-                        src={logoForeign}
-                        height="80"
-                        style={{ marginRight: '3px' }}></img>
-                    <img
-                        alt="logo"
-                        src={logoDaad}
-                        height="80"
-                        style={{ marginRight: '3px' }}></img>
-                    <img
-                        alt="logo"
-                        src={logoDip}
-                        height="40"
-                        style={{ marginRight: '3px' }}></img>
-                    <img
-                        alt="logo"
-                        src={logoLeibniz}
-                        height="60"
-                        style={{ marginRight: '3px' }}></img>
-                </div>
-            </div>
+        <FooterIcons />
+
         </div>
     );
 };
