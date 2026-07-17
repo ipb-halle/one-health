8dbbbd4e30a1e1d36c8fd07ab491a23fe161a8eb
Fabian Mauz <fmauz@ipb-halle.de>
2026-07-17 10:56:33 +0200

    feat: add REST endpoint for fetching neighbour entities

--- server/src/main/java/de/ipb_halle/server/api/EntityController.java
+++ server/src/main/java/de/ipb_halle/server/api/EntityController.java
@@ -5,26 +5,47 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import de.ipb_halle.server.api.interfaces.GraphController;
+import de.ipb_halle.server.data.dtos.EntityDTO;
+import de.ipb_halle.server.data.dtos.GraphDTO;
 import de.ipb_halle.server.data.dtos.LinkDTO;
+import de.ipb_halle.server.data.interfaces.IEntityRepository;
 import de.ipb_halle.server.services.interfaces.IEntityService;
+import java.util.ArrayList;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(path="api/entity")
+@RequestMapping(path = "api/entity")
 @CrossOrigin(origins = "*") // TODO: make this inheritable ? and use an env variable
 public class EntityController extends GraphController {
 
     private IEntityService entityService;
+    private IEntityRepository entityRepository;
 
-    public EntityController(IEntityService entityService) {
+    public EntityController(
+            IEntityService entityService,
+            IEntityRepository entityRepository
+    ) {
         super(entityService);
         this.entityService = entityService;
+        this.entityRepository = entityRepository;
     }
 
     @PostMapping("get-graph-references")
     public ResponseEntity<List<LinkDTO>> GetGraphReferences(@RequestBody List<String> nodes) {
         return new ResponseEntity<>(entityService.GetGraphReferences(nodes), HttpStatus.OK);
+    }
+
+    @GetMapping("getAdjacentEntities")
+    public ResponseEntity<List<EntityDTO>> getAdjacentEntities(@RequestParam String id) {
+
+        GraphDTO graphDto = entityService.GetAdjacentNodes(id, new ArrayList<>());
+
+        List<String> nodeIds = graphDto.getNodes().stream().map(node -> node.getId()).toList();
+
+        List<EntityDTO> entities = entityRepository.GetNodes(nodeIds);
+
+        return new ResponseEntity<>(entities, HttpStatus.OK);
     }
 
 }
