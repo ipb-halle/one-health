176f94ee0c4ddebd234dfaa108624509febe83d2
Fabian Mauz <fmauz@ipb-halle.de>
2026-07-17 13:54:10 +0200

    feat: add part of loading adjacent entities

--- client/src/store/EntityDetailStore.tsx
+++ client/src/store/EntityDetailStore.tsx
@@ -1,14 +1,28 @@
-import { Instance, types } from "mobx-state-tree";
+import { flow, Instance, types } from "mobx-state-tree";
 import { Entity } from "./Entity";
 
 export const EntityDetailStore = types
     .model({
-        selectedEntity: types.maybeNull(types.safeReference(Entity))
+        selectedEntity: types.maybeNull(types.safeReference(Entity)),
+        adjacentEntities: types.array(Entity)
     })
+    .actions((self) => ({
+        loadAdjacentEntitiesOfSelection: flow(function* (): any {
+            if (self.selectedEntity) {
+                const params = new URLSearchParams();
+                params.append("id", self.selectedEntity?.id);
+                const response = yield fetch(
+                    "api/entity/getAdjacentEntities?" + params.toString()
+                )
+            }
+        })
+    }))
 
     .actions((self) => ({
         setSelectedEntity(entity: Instance<typeof Entity>) {
             self.selectedEntity = entity;
-        }
+            self.loadAdjacentEntitiesOfSelection();
+        },
+
     }));
 
