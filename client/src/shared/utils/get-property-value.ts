e62c8a4fae2711f19b39176dbac49dbe83b851b2
Fabian Mauz <fmauz@ipb-halle.de>
2026-07-20 11:36:20 +0200

    refactor: add typing

--- client/src/shared/utils/get-property-value.ts
+++ client/src/shared/utils/get-property-value.ts
@@ -1,3 +1,12 @@
-export const getPropertyValue = (element: any, key: string) => {
-    return element?.properties?.find((e: any) => e.name === key)?.value || '';
+
+
+interface PropertyList {
+    properties: {
+        name: string,
+        value: string
+    }[]
+}
+
+export const getPropertyValue = (element: PropertyList, key: string) => {
+    return element?.properties?.find((e: { name: string }) => e.name === key)?.value || '';
 };
