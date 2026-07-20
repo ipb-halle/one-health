a8136585a909226e6a779c3db14ac76108049e56
Frank Broda <fbroda@ipb-halle.de>
2026-07-20 13:40:53 +0200

    add missing BaseURL

--- client/src/store/adapter/REST_ENDPOINTS.ts
+++ client/src/store/adapter/REST_ENDPOINTS.ts
@@ -1,1 +1,2 @@
-export const GET_ADJACENT_ENTITITES = "api/entity/getAdjacentEntities";
+const BASE_URL = import.meta.env.VITE_API_URL;
+export const GET_ADJACENT_ENTITITES = BASE_URL + "/api/entity/getAdjacentEntities";
