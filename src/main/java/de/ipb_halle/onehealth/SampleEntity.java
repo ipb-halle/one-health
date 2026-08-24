8011c5e31d10271553907036861e4034c8414cbc
Frank Broda <fbroda@ipb-halle.de>
2026-08-24 10:31:38 +0200

    add JPA annotations for sample case

--- src/main/java/de/ipb_halle/onehealth/SampleEntity.java
+++ src/main/java/de/ipb_halle/onehealth/SampleEntity.java
@@ -2,30 +2,57 @@ package de.ipb_halle.onehealth;
 
 import java.util.UUID;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "sample_entity")
 public class SampleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column
     private String name;
+
+    @Column
     private int value;
 
+    protected SampleEntity() {
+        // no-args constructor required by JPA
+    }
+
+    public SampleEntity(String name, int value) {
+        this.name = name;
+        this.value = value;
+    }
 
     public UUID getId() {
         return id;
     }
+
     public void setId(UUID id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
-    
+
     public int getValue() {
         return value;
     }
+
     public void setValue(int value) {
         this.value = value;
     }
-    
 }
