e74538c60cc5fcd5d8b43a4af826e12c84def33a
Hoda Anvari Kazemabad Local <halocal@compchem17.ipb-halle.de>
2026-09-02 15:15:36 +0200

    Remove obsolete user repository lookups

--- server/src/main/java/de/ipb_halle/server/postgre/repositories/UserRepository.java
+++ server/src/main/java/de/ipb_halle/server/postgre/repositories/UserRepository.java
@@ -3,13 +3,5 @@ package de.ipb_halle.server.postgre.repositories;
 import de.ipb_halle.server.postgre.models.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
-
-    Optional<UserEntity> findByEmail(String email);
-
-    Optional<UserEntity> findByOrcid(String orcid);
-
-    boolean existsByEmail(String email);
 }
