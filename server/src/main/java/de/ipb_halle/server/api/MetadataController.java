c54bcc332e8515608e9348caac87fdf770917225
Hoda Anvari Kazemabad Local <halocal@compchem17.ipb-halle.de>
2026-09-14 11:48:31 +0200

    Remove unused metadata REST endpoints

--- server/src/main/java/de/ipb_halle/server/api/MetadataController.java
+++ server/src/main/java/de/ipb_halle/server/api/MetadataController.java
@@ -13,41 +13,44 @@ import de.ipb_halle.server.services.interfaces.IMetadataService;
 import java.util.List;
 
 @RestController
-@RequestMapping(path="api/metadata")
+@RequestMapping(path = "api/metadata")
 @CrossOrigin(origins = "*") // TODO: make this inheritable ? and use an env variable
 public class MetadataController {
-
-
-    IMetadataService metadataService;
-
-    public MetadataController(IMetadataService metadataService){
-        this.metadataService = metadataService;
-    }
-
-    @GetMapping("all")
-    public ResponseEntity<GraphDTO>  GetAll(){
-        return new ResponseEntity<>(metadataService.GetAll(), HttpStatus.OK);
-
-    }
-
-    @GetMapping("as-options")
-    public ResponseEntity<List<SelectableOption<String>>> GetAllAsOptions() {
-        return new ResponseEntity<>(metadataService.GetAllAsOptions(), HttpStatus.OK);
-    }
-
-    @GetMapping("entity-type/{id}")
-    public ResponseEntity<MetadataElementDTO> GetEntityType(@PathVariable String id){
-        return new ResponseEntity<>(metadataService.GetEntityType(id), HttpStatus.OK);
-    }
-
-    @GetMapping("link-type/{id}")
-    public ResponseEntity<MetadataElementDTO> GetLinkType(@PathVariable String id){
-        return new ResponseEntity<>(metadataService.GetLinkType(id), HttpStatus.OK);
-    }
-
-    @GetMapping("summary")
-    public ResponseEntity<MetadataSummaryDTO> GetSummary()
-    {
-        return new ResponseEntity<>(metadataService.GetSummary(), HttpStatus.OK);
-    }
+    /*
+     * IMetadataService metadataService;
+     * 
+     * public MetadataController(IMetadataService metadataService) {
+     * this.metadataService = metadataService;
+     * }
+     * 
+     * @GetMapping("all")
+     * public ResponseEntity<GraphDTO> GetAll() {
+     * return new ResponseEntity<>(metadataService.GetAll(), HttpStatus.OK);
+     * 
+     * }
+     * 
+     * @GetMapping("as-options")
+     * public ResponseEntity<List<SelectableOption<String>>> GetAllAsOptions() {
+     * return new ResponseEntity<>(metadataService.GetAllAsOptions(),
+     * HttpStatus.OK);
+     * }
+     * 
+     * @GetMapping("entity-type/{id}")
+     * public ResponseEntity<MetadataElementDTO> GetEntityType(@PathVariable String
+     * id) {
+     * return new ResponseEntity<>(metadataService.GetEntityType(id),
+     * HttpStatus.OK);
+     * }
+     * 
+     * @GetMapping("link-type/{id}")
+     * public ResponseEntity<MetadataElementDTO> GetLinkType(@PathVariable String
+     * id) {
+     * return new ResponseEntity<>(metadataService.GetLinkType(id), HttpStatus.OK);
+     * }
+     * 
+     * @GetMapping("summary")
+     * public ResponseEntity<MetadataSummaryDTO> GetSummary() {
+     * return new ResponseEntity<>(metadataService.GetSummary(), HttpStatus.OK);
+     * }
+     */
 }
